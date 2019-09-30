1eaf2abee85d0895039ef1d7f3ea2f6dd2a01158
Simone Lanucara <simonelanucara@yahoo.it>
2019-09-30 14:56:53 +0200

    Update mvc_multiple_raster.py

--- mvc_multiple_raster.py
+++ mvc_multiple_raster.py
@@ -6,12 +6,8 @@ import numpy as np
 
 directory_to_check = "/home/jovyan/work/ClippingFeatures/" # Which directory do you want to start with?
 
-def subdirname():
-    for dir in os.listdir('/home/jovyan/work/ClippingFeatures/'):
-        dirname = dir
-
 def my_function(directory):
-      # get rasters' file names
+# get rasters' file names
     fnames = glob.glob('*.tif')
 
 # read general properties of the first raster (assuming all the rasters share these properties)
@@ -26,10 +22,10 @@ def my_function(directory):
 # read each raster and create a 3D array
     arrays = []
     for fn in fnames:
-       ds = gdal.Open(fn, 0)
-       arr = ds.ReadAsArray()  # 2D array (rows by columns)
-       arrays.append(arr)
-       del ds
+        ds = gdal.Open(fn, 0)
+        arr = ds.ReadAsArray()  # 2D array (rows by columns)
+        arrays.append(arr)
+        del ds
     arr = np.stack(arrays)  # 3D array (date by rows by columns)
 
 # mask the array to exclude values outside a defined range
@@ -43,8 +39,7 @@ def my_function(directory):
     arr = arr.filled(nd)
 
 # create the output raster
-#    out_fn = 'max_values_ndvi.tif'
-    out_fn = (str(dirname) + '_max_values_ndvi.tif')
+    out_fn = ('_max_values_ndvi.tif')
     driver = gdal.GetDriverByName('GTiff')
     out_ds = driver.Create(out_fn, xsize, ysize, 1, gdal.GDT_Float32)  # you might want to change the pixel type
     out_ds.SetGeoTransform(gt)
@@ -57,9 +52,10 @@ def my_function(directory):
     out_band.FlushCache()
     del out_ds, out_band
 
+
 # Get all the subdirectories of directory_to_check recursively and store them in a list:
-    directories = [os.path.abspath(x[0]) for x in os.walk(directory_to_check)]
-    directories.remove(os.path.abspath(directory_to_check)) # If you don't want your main directory included
+directories = [os.path.abspath(x[0]) for x in os.walk(directory_to_check)]
+directories.remove(os.path.abspath(directory_to_check)) # If you don't want your main directory included
 
 for i in directories:
       os.chdir(i)         # Change working Directory
