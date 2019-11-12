ef8da436790397bd29e92eeaed0e8e4d85b7d59b
Simone Lanucara <simonelanucara@yahoo.it>
2019-11-12 17:40:47 +0100

    Update bulk_import_getit.py

--- bulk_import_getit.py
+++ bulk_import_getit.py
@@ -1,15 +1,26 @@
 #to exec from host: docker-compose exec django python ./import.py
 import os
+from datetime import datetime
 
 directory_to_check = "/usr/src/app/EXTERNALDATA/get_it_aziende/"
+
 
 def importer(i):
 	cwd = os.getcwd()
 	nome_azienda = os.path.basename(cwd)
-	os.system("bash /usr/src/app/manage.sh importlayers " + "-u" + " " + str(nome_azienda) + " -p" + " " + i)
-	print (i)
-	print (nome_azienda)
-	
+	now = datetime.now()
+	startime = now.strftime("%d/%m/%Y %H:%M:%S")
+	print ("--- start " + nome_azienda + " ---")
+	print ("percorso directory" + " " + i)
+	print ("data e ora inizio" + " " + startime) 
+#	print ("inizio importazione dell'azienda" + " " + nome_azienda)
+	os.system("bash /usr/src/app/manage.sh importlayers " + "-v 3" + " " + "-u" + " " + str(nome_azienda) + " -p" + " " + i)
+	end = datetime.now()
+	endtime = end.strftime("%d/%m/%Y %H:%M:%S")
+	print ("--- end " + nome_azienda + " ---")
+	print ("data e ora fine" + " " + endtime)
+#	print ("fine importazione dell'azienda" + " " + nome_azienda)
+
 directories = [os.path.abspath(x[0]) for x in os.walk(directory_to_check)]
 directories.remove(os.path.abspath(directory_to_check)) # If you don't want your main directory 
 
