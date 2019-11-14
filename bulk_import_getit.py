e6b214d3be6c16c0b03f4d64d6e14874d94253e7
Simone Lanucara <simonelanucara@yahoo.it>
2019-11-14 16:00:26 +0100

    Update bulk_import_getit.py

--- bulk_import_getit.py
+++ bulk_import_getit.py
@@ -1,28 +1,38 @@
 #to exec from host: docker-compose exec django python ./import.py
 import os
+import subprocess
 from datetime import datetime
+import shutil
 
 directory_to_check = "/usr/src/app/EXTERNALDATA/get_it_aziende/"
 
-
 def importer(i):
-	cwd = os.getcwd()
-	nome_azienda = os.path.basename(cwd)
-	now = datetime.now()
-	startime = now.strftime("%d/%m/%Y %H:%M:%S")
-	print ("--- start " + nome_azienda + " ---")
-	print ("percorso directory" + " " + i)
-	print ("data e ora inizio" + " " + startime) 
-#	print ("inizio importazione dell'azienda" + " " + nome_azienda)
-	os.system("bash /usr/src/app/manage.sh importlayers " + "-v 3" + " " + "-u" + " " + str(nome_azienda) + " -p" + " " + i)
-	end = datetime.now()
-	endtime = end.strftime("%d/%m/%Y %H:%M:%S")
-	print ("--- end " + nome_azienda + " ---")
-	print ("data e ora fine" + " " + endtime)
-#	print ("fine importazione dell'azienda" + " " + nome_azienda)
+    cwd = os.getcwd()
+    nome_azienda = os.path.basename(cwd)
+    now = datetime.now()
+    startime = now.strftime("%d/%m/%Y %H:%M:%S")
+    print ("--- start " + nome_azienda + " ---")
+    print ("percorso directory" + " " + i)
+    print ("data e ora inizio" + " " + startime) 
+    print ("inizio importazione dell'azienda" + " " + nome_azienda)
+    importazione = ("bash", "/usr/src/app/manage.sh", "importlayers", "-v 3","-u",nome_azienda,"-p", i)
+    returned_value = subprocess.Popen(importazione, stdout=subprocess.PIPE)
+    output = returned_value.communicate()
+    #os.system("bash /usr/src/app/manage.sh importlayers " + "-v 3" + " " + "-u" + " " + str(nome_azienda) + " -p" + " " + i) #deprecato
+    for file in os.listdir(i):
+        ext = [".zip", ".shp", ".tif", ".sld"]
+        if file.endswith(tuple(ext)):
+            dataset = (os.path.join(i, file))
+            importpath = (os.path.join("/usr/src/app/EXTERNALDATA/imported/", nome_azienda))
+            shutil.move(dataset, importpath) #se voglio muovere i file
+        print ("dato archiviato " + importpath + "/" + file)
+    end = datetime.now()
+    endtime = end.strftime("%d/%m/%Y %H:%M:%S")
+    print ("data e ora fine" + " " + endtime)
+    print ("--- end " + nome_azienda + " ---")
 
 directories = [os.path.abspath(x[0]) for x in os.walk(directory_to_check)]
-directories.remove(os.path.abspath(directory_to_check)) # If you don't want your main directory 
+directories.remove(os.path.abspath(directory_to_check)) # per non includere la dir principale
 
 for i in directories:
 	os.chdir(i) # Change working Directory
